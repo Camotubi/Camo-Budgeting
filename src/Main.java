cc289fdd389596a6e8a2002583df15eebedae7ec
Carlos Tuñón <camotubi@gmail.com>
2016-08-14 23:55:44 -0500

    Add Account and Income Class

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,28 @@
 import java.awt.BorderLayout;
+import java.util.Date;
 import javax.swing.*;
-
+import money.Expense;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Expense exp = new Expense(1f, new Date(),"hola");
 		//Frame initialization
 		JFrame frame = new JFrame("Start");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//---
-		JLabel label1 = new JLabel("Hola");
+		JLabel label1 = new JLabel(exp.get_Date_str());
 		frame.getContentPane().add(label1, BorderLayout.CENTER);
 		
 		//---
-		JTextField textfield1 = new JTextField("");
+		/*JTextField textfield1 = new JTextField("");
 		frame.getContentPane().add(textfield1, BorderLayout.CENTER);
-		
+		*/
 		//---Frame Adjusting
 		frame.pack();
 		frame.setVisible(true);
+		
 		
 	}
 
--- src/money/Expense.java
+++ src/money/Expense.java
@@ -1,21 +1,53 @@
 package money;
+import java.util.Date;
 
 public class Expense {
 	private float amount;
-	private String date;
+	private Date date;
 	private String description;
 	
-	public Expense(float money, String date, String description)
+	public Expense(float money, Date date, String description)
 	{
 		this.amount = money;
 		this.date = date;
 		this.description = description;
 	}
 	
+	//---Setters
 	public void set_amount(float money)
 	{
 		this.amount = money;
 	}
 	
-
+	public void set_date(Date date)
+	{
+		this.date = date;
+	}
+	
+	public void set_description(String description)
+	{
+		this.description = description;
+	}
+	
+	//---Getters
+	public float get_amount()
+	{
+		return this.amount;
+	}
+	
+	public Date get_date()
+	{
+		return this.date;
+	}
+	
+	public String get_description()
+	{
+		return this.description;
+	}
+	
+	//---
+	public String get_Date_str()
+	{
+		return this.date.toString();
+	}
 }
